fd996cf804223891259c4ef465341da44b4ac3f6
Muhammad Arham <154342230+Ilupii17@users.noreply.github.com>
2024-11-23 01:58:56 +0800

    Add files via upload
    Modul Tugas 8

---
+++ Modul8_Tugas/build/classes/modul8_tugas/from_buku.rs
@@ -0,0 +1,8 @@
+from_buku$1
+from_buku
+from_buku$7
+from_buku$6
+from_buku$3
+from_buku$2
+from_buku$5
+from_buku$4
---
+++ Modul8_Tugas/src/modul8_tugas/koneksi.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+    private Connection konek;
+
+    public Connection getConnection() {
+        try {
+            konek = DriverManager.getConnection("jdbc:mysql://localhost/anggota","root", "");
+            System.out.println("Koneksi Berhasil");
+            //JOptionPane.showMessageDialog(null,"Koneksi Berhasil");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Koneksi Gagal" + e.getMessage());
+        }
+        return konek;
+    }
+
+    // Metode main untuk menguji koneksi
+    public static void main(String[] args) {
+        koneksi koneksTest = new koneksi();
+        Connection connection = koneksTest.getConnection();
+    }
+}
